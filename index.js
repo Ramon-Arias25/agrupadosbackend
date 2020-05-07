65a032cc66f24e5676dcf8d4ee25958f98138377
Ramon-Arias25 <56901379+Ramon-Arias25@users.noreply.github.com>
2020-05-07 11:16:34 -0300

    cambio de puerto al 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
  var mongoose = require('mongoose');
  var app = require ('./app');
- var port = 3000;
+ var port = 3800;
  //var uri = 'mongodb://localhost:27017/pooleddemo';
  var uri = 'mongodb+srv://agrupados:agrupados@cluster0-eo1s1.mongodb.net/test?retryWrites=true&w=majority';
  mongoose.set('useFindAndModify', false);
