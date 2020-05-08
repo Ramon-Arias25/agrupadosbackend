349ac834c0679a9a4ef03e18f35b6a501a0733a0
Ramon-Arias25 <56901379+Ramon-Arias25@users.noreply.github.com>
2020-05-07 23:30:19 -0300

    cambio de configuracion de puerto

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
  var mongoose = require('mongoose');
  var app = require ('./app');
- var port = 3800;
+ //var port = 3800;
  //var uri = 'mongodb://localhost:27017/pooleddemo';
  var uri = 'mongodb+srv://agrupados:agrupados@cluster0-eo1s1.mongodb.net/test?retryWrites=true&w=majority';
  mongoose.set('useFindAndModify', false);
  mongoose.Promise - global.Promise;
+ app.set('port', process.env.PORT || 3800);
  
  mongoose.connect( uri, {  
     useNewUrlParser: true,
@@ -14,10 +15,9 @@
 })
     .then(() => {
         console.log('++ DB is Connect');
-
         //create server
-        app.listen(port, () => {
-            console.log(uri, port );
+        app.listen(app.get('port'), () => {
+            console.log(uri, app.get('port') );
         });
     })
     .catch(err => console.log(err));
